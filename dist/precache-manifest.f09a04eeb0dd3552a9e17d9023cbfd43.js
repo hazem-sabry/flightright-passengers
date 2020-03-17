e8ff7a93a5b660e42138bab852ffb96422cdbc4d
Hazem Sabry <hazem.s@amaz.sa>
2020-03-17 12:16:24 +0200

    [INIT] Initialize the application structure

---
+++ dist/precache-manifest.f09a04eeb0dd3552a9e17d9023cbfd43.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "78e6b2bf4d8b378c70bc",
+    "url": "/css/app.d1741521.css"
+  },
+  {
+    "revision": "1d48fca2a449e3bbc4bf085c5b72dc89",
+    "url": "/img/logo.1d48fca2.png"
+  },
+  {
+    "revision": "9df278081cf246b10f817f144e587d2d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "78e6b2bf4d8b378c70bc",
+    "url": "/js/app.88dc49f3.js"
+  },
+  {
+    "revision": "d4b044134efc4e45329e",
+    "url": "/js/chunk-vendors.0b17fb2c.js"
+  },
+  {
+    "revision": "914148e5d46bae5f6849fdbc641ed0c0",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
